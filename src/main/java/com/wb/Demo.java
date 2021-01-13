b017a47a9ec9b988c555dc94af50f397598a86e8
liuli <liuli@moqipobing.com>
2021-01-13 20:58:04 +0800

    练习sql

--- src/main/java/com/wb/Demo.java
+++ src/main/java/com/wb/Demo.java
@@ -1,18 +1,19 @@
 package com.wb;
 
-import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 
 public class Demo {
     public static void main(String[] args) throws Exception{
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-        env.setParallelism(1);
-        env.socketTextStream("localhost",8888).map(new MapFunction<String, String>() {
-            @Override
-            public String map(String s) throws Exception {
-                return s;
-            }
-        }).print();
+        EnvironmentSettings settings = EnvironmentSettings.newInstance().useBlinkPlanner().inStreamingMode().build();
+        StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env,settings);
+
+        String s =
+"create table order_item (id String,name String) with ('connector' = 'jdbc','url' = 'jdbc:mysql://devtest.wb.sql.wb-intra.com:13306/spy?useUnicode=true&characterEncoding=UTF-8', 'driver' = 'com.mysql.jdbc.Driver','table-name' = 't_order_item','username' = 'test_liuli','password' = 'p!rM+LXMR9*e=')";
+        tableEnv.executeSql(s);
+
 
         env.execute("Flink Streaming Java API Skeleton");
     }
